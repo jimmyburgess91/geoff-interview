fcff1d7316c9749910d38821cd6f83fa2870db8d
Jimmy Burgess <jimmy.burgess@zume.com>
2019-11-24 00:01:41 -0800

    flood fill

---
+++ flood-fill.py
@@ -0,0 +1,14 @@
+grid = [
+        ['R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'],
+        ['R', 'R', 'Y', 'Y', 'Y', 'Y', 'Y', 'R', 'R']
+        ['R', 'R', 'Y', 'R', 'R', 'R', 'Y', 'R', 'R']
+        ['R', 'R', 'Y', 'R', 'R', 'R', 'Y', 'R', 'R']
+        ['R', 'R', 'Y', 'R', 'R', 'R', 'Y', 'R', 'R']
+        ['R', 'R', 'Y', 'R', 'R', 'R', 'Y', 'R', 'R']
+        ['R', 'R', 'Y', 'Y', 'Y', 'Y', 'Y', 'R', 'R']
+        ['R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R']
+        ['R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R']
+]
+
+def fill(row, col, new_color):
+    return # TODO practice for geoff
