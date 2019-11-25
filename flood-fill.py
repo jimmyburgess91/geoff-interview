f4878036f6192aff20566019555943dccf4fdf6a
Geoffe-Ga <geoffe.gallinger@gmail.com>
2019-11-25 14:47:44 -0800

    Pretty sure this works
    I was way over complicating it. Getting the shitty answer out of my system freed me up to do a better answer.

--- flood-fill.py
+++ flood-fill.py
@@ -15,43 +15,35 @@ grid = [
 def fill(row, col, new_color):
         global grid
         old_color = grid[row][col]
-        direction = ["row", -1]
-        navigate(row, col, direction, new_color, old_color)
+        navigate(row, col, new_color, old_color)
         return grid
 
 
-def navigate(row, col, direction, new_color, old_color):
+def navigate(row, col, new_color, old_color):
         global grid
         print("Checking grid[{}][{}]".format(row, col))
-        if grid[row][col] == old_color:
-                grid[row][col] = new_color
-                print("Just changed grid[{}][{}]".format(row, col))
+        directions = {"north": ["row", -1], "south": ["row", 1], "east": ["col", 1], "west": ["col", -1]}
+        for direction in directions.values():
+                if grid[row][col] == old_color:
+                        grid[row][col] = new_color
+                        print("Just changed grid[{}][{}]".format(row, col))
                 if direction[0] == "row":
-                        if grid[row + direction[1]][col] not in [old_color, new_color]:
-                                # check ahead for the boundary color and turn right
-                                direction[0] = "col"
-                                direction[1] = direction[1] * -1
-                                navigate(row, col + direction[1], direction, new_color, old_color)
-                        elif grid[row + direction[1]][col] == new_color:
-                                # turn right then left
-                                print("Current direction:", direction)
-                                navigate(row, col + (direction[1]*-1), direction, new_color, old_color)
-                        else:
-                                # keep going in the same direction
-                                navigate(row + direction[1], col, direction, new_color, old_color)
+                        try:
+                                if grid[row + direction[1]][col] != old_color:
+                                        continue
+                                else:
+                                        navigate(row + direction[1], col, new_color, old_color)
+                        except IndexError:
+                                continue
                 if direction[0] == "col":
-                        if grid[row][col + direction[1]] not in [old_color, new_color]:
-                                # check ahead for the boundary color and turn right
-                                direction[0] = "row"
-                                navigate(row + direction[1], col, direction, new_color, old_color)
-                        elif grid[row][col + direction[1]] == new_color:
-                                # turn right then left
-                                print("Current direction:", direction)
-                                direction[1] = direction[1] * -1
-                                navigate(row + (direction[1]*-1), col, direction, new_color, old_color)
-                        else:
-                                # keep going in the same direction
-                                navigate(row, col + direction[1], direction, new_color, old_color)
+                        try:
+                                if grid[row][col + direction[1]] != old_color:
+                                        continue
+                                else:
+                                        navigate(row, col + direction[1], new_color, old_color)
+                        except IndexError:
+                                continue
+
 
         return
 
@@ -61,33 +53,32 @@ def main():
         print("GRID:")
         pprint(grid)
 
-        # new_color = input("What color would you like to change to?")
-        # while True:
-        #         try:
-        #                 row = int(input("What row would you like to start on?\n"))
-        #                 test_row = grid[row][0]
-        #                 break
-        #         except ValueError:
-        #                 print("That entry can't be converted to a number. Please try again.")
-        #         except IndexError:
-        #                 print("That entry is out of range. Please try again.")
-        #                 continue
-        #
-        # while True:
-        #         try:
-        #                 col = int(input("What row would you like to start on?\n"))
-        #                 col = grid[0][col]
-        #                 break
-        #         except ValueError:
-        #                 print("That entry can't be converted to a number. Please try again.")
-        #         except IndexError:
-        #                 print("That entry is out of range. Please try again.")
-        #                 continue
-        # new_color = input("And what color should we fill with?\n")
+        while True:
+                try:
+                        row = int(input("What row would you like to start on?\n"))
+                        test_row = grid[row][0]
+                        break
+                except ValueError:
+                        print("That entry can't be converted to a number. Please try again.")
+                except IndexError:
+                        print("That entry is out of range. Please try again.")
+                        continue
 
-        new_color = "G"
-        row = 3
-        col = 3
+        while True:
+                try:
+                        col = int(input("What row would you like to start on?\n"))
+                        test_col = grid[0][col]
+                        break
+                except ValueError:
+                        print("That entry can't be converted to a number. Please try again.")
+                except IndexError:
+                        print("That entry is out of range. Please try again.")
+                        continue
+        new_color = input("And what color should we fill with?\n")
+
+        # new_color = "G"
+        # row = 7
+        # col = 3
 
         result = fill(row, col, new_color)
 
