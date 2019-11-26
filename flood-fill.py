30a7b310319f4b1a270dd91f4a4ed446c91e7620
Geoffe-Ga <geoffe.gallinger@gmail.com>
2019-11-26 12:24:45 -0800

    Cut down on directions, merged navigate and fill functions

--- flood-fill.py
+++ flood-fill.py
@@ -12,39 +12,24 @@ grid = [
 ]
 
 
-def fill(row, col, new_color):
-        old_color = grid[row][col]
-        navigate(row, col, new_color, old_color)
-        return grid
-
-
-def navigate(row, col, new_color, old_color):
-
+def fill(row, col, new_color, old_color):
         print("Checking grid[{}][{}]".format(row, col))
-        directions = {"north": ["row", -1], "south": ["row", 1], "east": ["col", 1], "west": ["col", -1]}
-        for direction in directions.values():
+        directions = [(-1, 0),  # north
+                      (1, 0),   # south
+                      (0, 1),   # east
+                      (0, -1)   # west
+                      ]
+
+        for direction in directions:
                 if grid[row][col] == old_color:
                         grid[row][col] = new_color
                         print("Just changed grid[{}][{}]".format(row, col))
-                if direction[0] == "row":
-                        try:
-                                if grid[row + direction[1]][col] != old_color:
-                                        continue
-                                else:
-                                        navigate(row + direction[1], col, new_color, old_color)
-                        except IndexError:
-                                continue
-                if direction[0] == "col":
-                        try:
-                                if grid[row][col + direction[1]] != old_color:
-                                        continue
-                                else:
-                                        navigate(row, col + direction[1], new_color, old_color)
-                        except IndexError:
-                                continue
-
-
-        return
+                try:
+                        if grid[row + direction[0]][col + direction[1]] == old_color:
+                                fill(row + direction[0], col + direction[1], new_color, old_color)
+                except IndexError:
+                        continue
+        return grid
 
 
 def main():
@@ -54,7 +39,7 @@ def main():
         while True:
                 try:
                         row = int(input("What row would you like to start on?\n"))
-                        test_row = grid[row][0]
+                        test_row = grid[row]
                         break
                 except ValueError:
                         print("That entry can't be converted to a number. Please try again.")
@@ -65,7 +50,7 @@ def main():
         while True:
                 try:
                         col = int(input("What row would you like to start on?\n"))
-                        test_col = grid[0][col]
+                        old_color = test_row[col]
                         break
                 except ValueError:
                         print("That entry can't be converted to a number. Please try again.")
@@ -78,7 +63,7 @@ def main():
         # row = 7
         # col = 3
 
-        result = fill(row, col, new_color)
+        result = fill(row, col, new_color, old_color)
 
         print("\nFILLED:")
         pprint(result)
